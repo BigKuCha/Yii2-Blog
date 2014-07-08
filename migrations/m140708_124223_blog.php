eb09ba38829018de7ee33c866a08b17ce827297d
BigKuCha <nevermore1989@vip.qq.com>
2014-07-08 20:43:39 +0800

    Migration

---
+++ migrations/m140708_124223_blog.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m140708_124223_blog extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m140708_124223_blog cannot be reverted.\n";
+
+        return false;
+    }
+}
